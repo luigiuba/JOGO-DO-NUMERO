bb7179b744ec7e3a5c38680a295e7a818897d374
luigiuba <luigibaezo@gmail.com>
2025-08-14 15:58:59 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+alert('Seja bem vindo ao GAME MORTAL DO NUMERO SECRETO!');
+nomeUser = prompt('Antes de começarmos gostariamos de perguntar qual o seu nome')
+alert(`Saudações ${nomeUser}`)
+console.log('boas vindas dev ' + nomeUser)
+
+/// aleatoriza um numero entre 30 inteiro com parseint
+let numeroMaximo = 100;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log('o numero aqui dev shhhhh ' + numeroSecreto);
+let chute;
+let tentativas = 1;
+
+// enquanto != nao for igual
+while (chute != numeroSecreto) {
+    chute = prompt(`qual o teu chute ${nomeUser} ? ta entre 1 e ${numeroMaximo}`);
+if (chute == numeroSecreto) 
+    {
+     break; 
+} else {
+    if (chute < numeroSecreto) {
+        alert('Pouquinho mais que ' + chute);
+    } else {
+        alert('Pouquinho menos que ' + chute);
+    }
+    // para atribuir mais 1 utilizar ++
+    tentativas ++;
+    }
+}
+let palavraTentativa = tentativas > 1? 'tentativas' : 'tentativa'
+alert(`Boa!! ${nomeUser}, o numero era ${numeroSecreto} mas tu usou ${tentativas} ${palavraTentativa
+}`);
